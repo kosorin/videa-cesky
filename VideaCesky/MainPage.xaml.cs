ceaad194493bfb658cbae407b6d138bb3d323f5c
David Kosorin <david@kosorin.net>
2014-07-07 06:19:53 +0200

    Lepší debugování

--- VideaCesky/MainPage.xaml.cs
+++ VideaCesky/MainPage.xaml.cs
@@ -23,6 +23,33 @@ namespace VideaCesky
         public MainPage()
         {
             this.InitializeComponent();
+#if DEBUG
+            Loaded += Page_Loaded;
+#endif
         }
+
+#if DEBUG
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            ListPickerFlyout fo = new ListPickerFlyout();
+            fo.ItemsSource = new Dictionary<string, string>() 
+            { 
+                {"normální", "http://www.videacesky.cz/serialy-online-zdarma/odvazni-valecnici-2x06-loutkovy-horor"},
+                {"playlist 1", "http://www.videacesky.cz/ostatni-zabavna-videa/conan-policejnim-straznikem"},
+                {"playlist 2", "http://www.videacesky.cz/talk-show-rozhovory/arnold-schwarzenegger-u-jimmyho-fallona"},
+                {"více videí", "http://www.videacesky.cz/talk-show-rozhovory/russell-brand-u-conana-obriena"},
+                {"xml", "http://www.videacesky.cz/reklamy-reklamni-spot-video/nekompromisni-maskot"},
+            };
+            fo.ItemsPicked += (s2, e2) =>
+            {
+                if (e2.AddedItems.Count > 0)
+                {
+                    Frame.Navigate(typeof(VideoPage), ((KeyValuePair<string, string>)e2.AddedItems[0]).Value);
+                }
+            };
+
+            await fo.ShowAtAsync(ContentRoot);
+        }
+#endif
     }
 }
